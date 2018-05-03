a945c09855de6d324b36c0faa28d1b8e2bb839e4
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-05-03 20:27:37 +0300

    2.13* maiden tiedot klikkaamalla

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import axios from 'axios'
 
-const Country = ({ country }) => <p>{country.name}</p>
+const Country = ({ country, action }) => {
+  return (
+    <div onClick={action}>
+      {country.name}
+    </div>
+  )
+}
 
 const CountrySpecs = ({ country }) => {
   return (
@@ -9,7 +15,7 @@ const CountrySpecs = ({ country }) => {
       <h2>{country.name}</h2>
       <h4>capital: {country.capital}</h4>
       <h4>population: {country.population}</h4>
-      <img src={country.flag} height="300" width="400" alt="kuva" />
+      <img src={country.flag} height="300" width="400" alt="" />
     </div>
   )
 }
@@ -28,13 +34,12 @@ class App extends React.Component {
     this.state = {
       countries: [],
       searchCountries: [],
+      showCountry: [],
       filter:''
     }
-    console.log('constructor')
   }
 
   componentDidMount() {
-    console.log('will mount')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
@@ -45,29 +50,55 @@ class App extends React.Component {
 
   searchInput = (event) => {
     this.setState({ filter: event.target.value })
-    const re = new RegExp(event.target.value.toLowerCase(), 'g')
-    const toLower = (str) => str.toLowerCase()
-    let searchCountries = this.state.countries.filter(x => toLower(x.name).match(re) )
-    console.log(searchCountries)
-      this.setState({ searchCountries })
+    console.log(event.target.value);
+    if (event.target.value.length > 0) {
+      const re = new RegExp(event.target.value.toLowerCase(), 'g')
+      const toLower = (str) => str.toLowerCase()
+      let searchCountries = this.state.countries.filter(x => toLower(x.name).match(re) )
+      console.log(searchCountries)
+        this.setState({
+          searchCountries,
+          showCountry: ''
+        })
+    } else {
+      this.setState({
+        searchCountries: '',
+        showCountry: ''
+      })
+    }
    }
 
   render() {
     console.log(this.state.searchCountries)
+
+    const showCountry = () => {
+      if (this.state.showCountry.name) {
+        console.log('NÄYTÄÄ')
+        return <CountrySpecs country={this.state.showCountry} />
+      }
+    }
+
+    const setCountry = (country) => () => {
+      this.setState({ showCountry: country })
+    }
+
     const showCountries = () => {
       if (1 < this.state.searchCountries.length && this.state.searchCountries.length < 10) {
-          return this.state.searchCountries.map((c, i) => <Country key={i} country={c} />)
+        return this.state.searchCountries.map((c, i) => <Country key={i} country={c} action={setCountry(c)} />)
       } else if (this.state.searchCountries.length > 10) {
         return 'too many matches, specify another filter'
       } else if (1 === this.state.searchCountries.length) {
         return <CountrySpecs country={this.state.searchCountries[0]} />
+      } else {
+        return ''
       }
     }
-    
+
     return (
       <div>
         <SearchForm filtter={this.state.filter} inputTxt={this.searchInput} />
 
+        {showCountry()}
         <div>
           {showCountries()}
         </div>
