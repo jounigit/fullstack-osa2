e74ac7eb6bb51e93104baffa2bebb5cd0ac28d31
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-05-03 11:56:16 +0300

    2.8 puhelinluettelo osa 3

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
 import React from 'react';
 
+const Henkilo = ({ henkilo }) => <p>{henkilo.nimi} {henkilo.numero}</p>
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas' },
-        { name: 'Esko Ukkonen' }
+      henkilot: [
+        {
+          nimi: 'Arto Hellas',
+         numero: '123499'
+       }
       ],
-      newName: ''
+      uusiNimi: '',
+      uusiNumero: ''
     }
   }
 
   addPerson = (event) => {
-   event.preventDefault()
-   const names = this.state.persons.map(person => person.name)
+    event.preventDefault()
+    const nimet = this.state.henkilot.map(henkilo => henkilo.nimi)
+    if (nimet.includes(this.state.uusiNimi)) {
+      this.setState({
+        uusiNimi: '',
+        uusiNumero: ''
+       })
+       console.log('Nimi on jo käytössä!')
+       alert("Nimi on jo käytössä!")
+    } else {
+      const personObject = {
+        nimi: this.state.uusiNimi,
+        numero: this.state.uusiNumero
+      }
 
-   if (names.includes(this.state.newName)) {
-     this.setState({ newName: '' })
-      console.log('Nimi on jo käytössä!')
-      alert("Nimi on jo käytössä!")
-   } else {
-     const personObject = {
-       name: this.state.newName
-     }
-     const persons = this.state.persons.concat(personObject)
+      const henkilot = this.state.henkilot.concat(personObject)
 
-     this.setState({
-       persons,
-       newName: ''
-     })
-   }
- }
+      this.setState({
+        henkilot,
+        uusiNimi: '',
+        uusiNumero: ''
+      })
+    }
+
+  }
 
   handlePersonChange = (event) => {
     console.log(event.target.value)
-    this.setState({ newName: event.target.value })
+    this.setState({ uusiNimi: event.target.value })
+  }
+
+  numeroIput = (event) => {
+    console.log(event.target.value)
+    this.setState({ uusiNumero: event.target.value })
   }
 
   render() {
@@ -45,7 +61,11 @@ class App extends React.Component {
         <form onSubmit={this.addPerson}>
           <div>
           nimi:
-          <input value={this.state.newName} onChange={this.handlePersonChange} />
+          <input value={this.state.uusiNimi} onChange={this.handlePersonChange} />
+          </div>
+          <div>
+          numero:
+          <input value={this.state.uusiNumero} onChange={this.numeroIput} />
           </div>
           <button type="submit">tallenna</button>
         </form>
@@ -53,7 +73,7 @@ class App extends React.Component {
         <h2>Numerot</h2>
 
         <div>
-          {this.state.persons.map((person, i) => <p key={i}>{person.name}</p>)}
+        {this.state.henkilot.map((henkilo, i) => <Henkilo key={i} henkilo={henkilo} />)}
         </div>
 
       </div>
