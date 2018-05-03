2b2885f862014bf1f9ade2308d0297cee75b0269
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-05-03 20:59:14 +0300

    2.14 puhelinluettelo osa 7

--- src/App.js
+++ src/App.js
@@ -1,106 +1,136 @@
 import React from 'react';
 import axios from 'axios'
 
-const Country = ({ country, action }) => {
-  return (
-    <div onClick={action}>
-      {country.name}
-    </div>
-  )
-}
-
-const CountrySpecs = ({ country }) => {
-  return (
-    <div>
-      <h2>{country.name}</h2>
-      <h4>capital: {country.capital}</h4>
-      <h4>population: {country.population}</h4>
-      <img src={country.flag} height="300" width="400" alt="" />
-    </div>
-  )
-}
+const Person = ({ person }) => <p>{person.name} {person.number}</p>
 
 const SearchForm = ({ filtter, inputTxt}) => {
   return (
     <div>
-    find countries:  <input value={filtter} onChange={inputTxt} />
+    rajaa näytettäviä:  <input value={filtter} onChange={inputTxt} />
     </div>
   )
+}
+
+const AddForm = ({add, name, number, inputP, inputN}) => {
+ return (
+   <form onSubmit={add}>
+     <div>
+     nimi:
+     <input value={name} onChange={inputP} />
+     </div>
+     <div>
+     numero:
+     <input value={number} onChange={inputN} />
+     </div>
+     <button type="submit">tallenna</button>
+   </form>
+ )
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      countries: [],
-      searchCountries: [],
-      showCountry: [],
-      filter:''
+      persons: [],
+      searchPersons: [],
+      newName: '',
+      newNumber: '',
+      filter: '',
+      showAll: true
     }
+    console.log('constructor')
   }
 
   componentDidMount() {
+    console.log('will mount')
     axios
-      .get('https://restcountries.eu/rest/v2/all')
+      .get('http://localhost:3001/persons')
       .then(response => {
         console.log('promise fulfilled')
-        this.setState({ countries: response.data })
+        this.setState({ persons: response.data })
       })
    }
+
+   personExist = (name) => {
+     const names = this.state.persons.map(person => person.name)
+     if (names.includes(name)) {
+       this.setState({
+         newName: '',
+         newNumber: ''
+        })
+        console.log('Nimi on jo käytössä!')
+        alert("Nimi on jo käytössä!")
+        return true
+     }
+   }
+
+  addPerson = (event) => {
+    event.preventDefault()
+    if ( !this.personExist(this.state.newName) ) {
+      const personObject = {
+        name: this.state.newName,
+        number: this.state.newNumber,
+        id: this.state.persons.length + 1
+      }
+
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          this.setState({
+            persons: this.state.persons.concat(personObject),
+            newName: '',
+            newNumber: '',
+            filter: '',
+            showAll: true
+          })
+        })
+    }
+
+  }
+
+  personInput = (event) =>  this.setState({
+    newName: event.target.value,
+    filter: '',
+    showAll: true
+  })
+
+  numberIput = (event) => this.setState({
+    newNumber: event.target.value,
+    filter: '',
+    showAll: true
+  })
 
   searchInput = (event) => {
     this.setState({ filter: event.target.value })
-    console.log(event.target.value);
-    if (event.target.value.length > 0) {
-      const re = new RegExp(event.target.value.toLowerCase(), 'g')
-      const toLower = (str) => str.toLowerCase()
-      let searchCountries = this.state.countries.filter(x => toLower(x.name).match(re) )
-      console.log(searchCountries)
-        this.setState({
-          searchCountries,
-          showCountry: ''
-        })
-    } else {
-      this.setState({
-        searchCountries: '',
-        showCountry: ''
-      })
-    }
+    const re = new RegExp(event.target.value.toLowerCase(), 'g')
+    const toLower = (str) => str.toLowerCase()
+    let searchPersons = this.state.persons.filter(x => toLower(x.name).match(re) )
+    console.log(searchPersons)
+    this.setState({ searchPersons, showAll: false })
    }
 
   render() {
-    console.log(this.state.searchCountries)
-
-    const showCountry = () => {
-      if (this.state.showCountry.name) {
-        console.log('NÄYTÄÄ')
-        return <CountrySpecs country={this.state.showCountry} />
-      }
-    }
-
-    const setCountry = (country) => () => {
-      this.setState({ showCountry: country })
-    }
-
-    const showCountries = () => {
-      if (1 < this.state.searchCountries.length && this.state.searchCountries.length < 10) {
-        return this.state.searchCountries.map((c, i) => <Country key={i} country={c} action={setCountry(c)} />)
-      } else if (this.state.searchCountries.length > 10) {
-        return 'too many matches, specify another filter'
-      } else if (1 === this.state.searchCountries.length) {
-        return <CountrySpecs country={this.state.searchCountries[0]} />
-      } else {
-        return ''
-      }
-    }
+    const showPersons =
+    this.state.showAll ?
+    this.state.persons :
+    this.state.searchPersons
+    console.log('showPersons: ', showPersons )
 
     return (
       <div>
         <SearchForm filtter={this.state.filter} inputTxt={this.searchInput} />
 
-        {showCountry()}
+        <h2>Puhelinluettelo</h2>
+        <AddForm
+        add={this.addPerson}
+        name={this.state.newName}
+        number={this.state.newNumber}
+        inputP={this.personInput}
+        inputN={this.numberIput} />
+
+        <h2>Numerot</h2>
         <div>
-          {showCountries()}
+          {showPersons.map(p => <Person key={p.id} person={p} />)}
         </div>
       </div>
     )
